40bdd81abb852f21ac5ac5cc6b98b8510664bf89
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-07 19:37:28 +1100

    Load reset and fonts into app styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import { globleStyles } from './ui-library/global';
+
+const GlobalStyles = createGlobalStyle`
+  ${globleStyles}
+`;
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;

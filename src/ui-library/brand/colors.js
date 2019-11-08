bb6edbc61d658ba5b7bbb3e1adaaeb7929148386
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-09 10:38:19 +1100

    Update colors

--- src/ui-library/brand/colors.js
+++ src/ui-library/brand/colors.js
@@ -1,5 +1,8 @@
 export const brandColors = {
   red: '#EB5757',
   green: '#22AA4E',
-  darkGrey: '#1C1C1C'
+  darkGrey: '#1C1C1C',
+  lightGrey: '#eeeeee',
+  grey: '#555555',
+  white: '#FFFFFF'
 };

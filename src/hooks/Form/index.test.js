716dcac29b74a5bb1ec70a709f83c43ab753cf34
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-09 23:34:14 +1100

    Fix semicolon

--- src/hooks/Form/index.test.js
+++ src/hooks/Form/index.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook, act } from '@testing-library/react-hooks';
 import { useAddressBookFormHook } from './index';
 
 describe('USE FORM HOOK', () => {
